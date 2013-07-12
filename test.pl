29e85ade0c06339dde9a0f01eb54cc671045b541
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2013-07-12 19:01:25 +0530

    fixed name change

--- test.pl
+++ test.pl
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 use feature ':5.12';
 
-use WWW::Firebase;
+use Firebase::Auth;
 
 my $tk="0014ae3b1ded44de9d9f6fc60dfd1c64";
-my $firebase = WWW::Firebase->new ( token =>$tk);
+my $firebase = Firebase::Auth->new ( token =>$tk);
 my %custom_data = ("id" => "example");
 my $token = $firebase->create_token ( \%custom_data );
  say( "token $token");
